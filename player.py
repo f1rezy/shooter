2b614e1bbb24221c5e2018fc55605c3fada5958b
Daniil Litvinenko <danil.fire@bk.ru>
2023-01-05 03:33:18 +0700

    Create class player

---
+++ player.py
@@ -0,0 +1,38 @@
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(pygame.Color("Green"))
+        self.rect = self.image.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+        self.speedx = 0
+        self.speedy = 0
+
+    def update(self):
+        self.speedx = 0
+        self.speedy = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_a]:
+            self.speedx = -4
+        if keystate[pygame.K_d]:
+            self.speedx = 4
+
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+        if keystate[pygame.K_w]:
+            self.speedy = -4
+        if keystate[pygame.K_s]:
+            self.speedy = 4
+
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
