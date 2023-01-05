6a39c37ec5f7e5037046e4210667610cbd080dec
Daniil Litvinenko <danil.fire@bk.ru>
2023-01-05 23:24:38 +0700

    microupdates

--- bullet.py
+++ bullet.py
@@ -2,40 +2,32 @@ import pygame
 
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, *group):
-        super().__init__(*group)
-        self.image = pygame.Surface((5, 5))
+    def __init__(self, group, coords, rect):
+        super().__init__(group)
+        self.image = pygame.Surface((10, 10))
         self.image.fill(pygame.Color("White"))
-        self.rect = self.image.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-        self.speedx = 0
-        self.speedy = 0
+        self.rect = self.image.get_rect(center=rect)
+        self.x, self.y = coords
 
     def update(self):
-        self.speedx = 0
-        self.speedy = 0
-        keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_a]:
-            self.speedx = -4
-        if keystate[pygame.K_d]:
-            self.speedx = 4
+        if self.rect.right > 1280:
+            self.kill()
 
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
         if self.rect.left < 0:
-            self.rect.left = 0
-
-        if keystate[pygame.K_w]:
-            self.speedy = -4
-        if keystate[pygame.K_s]:
-            self.speedy = 4
+            self.kill()
 
         if self.rect.top < 0:
-            self.rect.top = 0
+            self.kill()
 
-        if self.rect.bottom > HEIGHT:
-            self.rect.bottom = HEIGHT
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
+        if self.rect.bottom > 960:
+            self.kill()
 
-    def __call__(self, args):
-        return
+        if self.rect.x < self.x:
+            self.rect.x += 1
+        else:
+            self.rect.x -= 1
+
+        if self.rect.y < self.y:
+            self.rect.y += 1
+        else:
+            self.rect.y -= 1
--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import pygame
 from player import Player
+from bullet import Bullet
 
 size = WIDTH, HEIGHT = 1280, 960
 FPS = 60
 
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption("")
+    pygame.display.set_caption("shooter")
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
-    all_sprites = pygame.sprite.Group()
-    player = Player(all_sprites)
+    player_group = pygame.sprite.Group()
+    bullets_group = pygame.sprite.Group()
+    player = Player(player_group)
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        all_sprites.update()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                Bullet(bullets_group, event.pos, player.rect.center)
         screen.fill(pygame.Color("Black"))
-        all_sprites.draw(screen)
+        player_group.draw(screen)
+        player_group.update()
+        bullets_group.draw(screen)
+        bullets_group.update()
         pygame.display.flip()
         clock.tick(FPS)
