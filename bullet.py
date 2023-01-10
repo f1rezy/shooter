d8b83dbd8cc950a5967bdad836d78f9454b6095a
Matvey <atallab@ya.ru>
2023-01-10 22:45:11 +0700

    add_enemy

--- bullet.py
+++ bullet.py
@@ -2,32 +2,13 @@ import pygame
 
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, group, coords, rect):
-        super().__init__(group)
+    def __init__(self, rect):
+        super().__init__()
         self.image = pygame.Surface((10, 10))
         self.image.fill(pygame.Color("White"))
         self.rect = self.image.get_rect(center=rect)
-        self.x, self.y = coords
 
     def update(self):
-        if self.rect.right > 1280:
-            self.kill()
-
-        if self.rect.left < 0:
-            self.kill()
-
         if self.rect.top < 0:
             self.kill()
-
-        if self.rect.bottom > 960:
-            self.kill()
-
-        if self.rect.x < self.x:
-            self.rect.x += 1
-        else:
-            self.rect.x -= 1
-
-        if self.rect.y < self.y:
-            self.rect.y += 1
-        else:
-            self.rect.y -= 1
+        self.rect.y -= 4
