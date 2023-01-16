8ef1820989772d9df0a52f5fdf6550649b382aaf
Matvey <atallab@ya.ru>
2023-01-16 23:24:06 +0700

    add start/end screen

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import sys
+
 import pygame
 from player import Player
 from bullet import Bullet
 from enemy import Enemy
 from load_image import load_image
+from screen_start import screen_start
+from screen_end import screen_end
+from text import text
+from write_res import write
 
 SIZE = WIDTH, HEIGHT = 640, 480
 FPS = 60
@@ -15,19 +21,11 @@ enemy_images = {
     '6': load_image('enemy3.png'),
 }
 
-
-def text(surf, text, size, x, y):
-    font = pygame.font.Font(font_name, size)
-    text_surface = font.render(text, True, pygame.Color("white"))
-    text_rect = text_surface.get_rect()
-    text_rect.midtop = (x, y)
-    surf.blit(text_surface, text_rect)
-
-
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption("shooter")
     screen = pygame.display.set_mode(SIZE)
+
     clock = pygame.time.Clock()
     timer = 1000
 
@@ -45,18 +43,25 @@ if __name__ == '__main__':
     level = 1
     speed = 2
 
+    screen_start(screen)
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                Bullet(bullets_group, (player.rect.centerx, player.rect.top))
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    Bullet(bullets_group, (player.rect.centerx, player.rect.top))
+
             if event.type == time:
                 enemy = Enemy(enemy_images[str(level)])
                 enemy_group.add(enemy)
 
         if player.hp <= 0:
+            print(global_score)
+            write(global_score)
+            screen_end(screen)
             running = False
 
         if score == 10:
@@ -82,8 +87,8 @@ if __name__ == '__main__':
         bullets_group.update()
         enemy_group.draw(screen)
         enemy_group.update(speed, player_group)
-        text(screen, str(score), 36, WIDTH / 2, 10)
-        text(screen, f"level: {level}", 36, 75, 10)
-        text(screen, f"hp: {player.hp}", 36, 575, 10)
+        text(screen, str(score), 36, WIDTH / 2, 10, font_name)
+        text(screen, f"level: {level}", 36, 75, 10, font_name)
+        text(screen, f"hp: {player.hp}", 36, 575, 10, font_name)
         pygame.display.flip()
         clock.tick(FPS)
---
+++ write_res.py
@@ -0,0 +1,3 @@
+def write(global_score):
+    f = open('results.txt', 'w')
+    f.write(str(global_score))
